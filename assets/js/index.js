90a1a544cee7f6de1763022fa37dea5a13bddf43
gajjokj <160743051+gajjokj@users.noreply.github.com>
2025-02-15 18:20:16 +0000

    port?

---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+/* Theme mode */
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("fa-moon")) {
+    darkmode.classList.replace("fa-moon", "fa-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("fa-sun", "fa-moon");
+    document.body.classList.remove("active");
+  }
+};
+
+/* Infinite scroll images */
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
